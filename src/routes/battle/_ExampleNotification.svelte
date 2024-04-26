8723a5323937c0df5b546b363460c4939a09d1bf
nwpcgn <119536313+nwpcgn@users.noreply.github.com>
2024-04-26 19:54:34 +0200

    ua

--- src/routes/battle/_ExampleNotification.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	export let i
-	export let notificationIndex
-	export let type = ''
-</script>
-
-<div class="alert {type}" style="--index: {notificationIndex} ">
-	<span>Notification number {i}</span>
-</div>
